ebd31f829e8fcffad31bbfdc5ba5f21218093189
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2024-12-12 10:47:42 +0200

    Refactors application structure and adds sidebar components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
-import App from "./App";
+import { ThemeProvider } from "./components/theme-provider";
+import App from "./app";
 
 const container = document.getElementById("root");
 
@@ -9,6 +10,8 @@ const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
