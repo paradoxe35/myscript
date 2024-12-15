c9f2c9e7543d7632cc295d0276e7e097951d7e03
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2024-12-15 14:21:29 +0200

    Adds sidebar functionality for managing Notion pages
    Introduces a new action button in the sidebar for adding local pages using a plus icon.
    
    Enhances the Notion pages section with a separator for improved visual clarity and organizes user interface elements for better accessibility.
    
    Implements persistent storage for the Notion pages using Zustand middleware, enabling state persistence across sessions.
    
    Relates to user experience improvements in the sidebar interaction.

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Plus } from "lucide-react";
 
 import { SearchForm } from "@/components/search-form";
 import { SettingsButton } from "@/components/settings-button";
@@ -6,6 +7,7 @@ import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
+  SidebarGroupAction,
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarHeader,
@@ -16,6 +18,7 @@ import {
 } from "@/components/ui/sidebar";
 import { useNotionPagesStore } from "@/store/notion-pages";
 import { cn } from "@/lib/utils";
+import { Separator } from "./ui/separator";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const notionPagesStore = useNotionPagesStore();
@@ -34,7 +37,19 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarContent>
         {/* Local pages */}
         <SidebarGroup>
-          <SidebarGroupLabel>{"Local pages"}</SidebarGroupLabel>
+          <SidebarMenuButton className="px-0 justify-between group transition">
+            <SidebarGroupLabel>{"Local pages"}</SidebarGroupLabel>
+
+            <SidebarGroupAction
+              asChild
+              className="opacity-0 group-hover:opacity-100 transition-opacity hover:bg-sidebar/60"
+            >
+              <a href="#">
+                <Plus />
+              </a>
+            </SidebarGroupAction>
+          </SidebarMenuButton>
+
           {/* <SidebarGroupContent>
             <SidebarMenu>
               {item.items.map((item) => (
@@ -50,26 +65,30 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
         {/* Notion pages */}
         {notionPages.length > 0 && (
-          <SidebarGroup>
-            <SidebarGroupLabel>{"Notion pages"}</SidebarGroupLabel>
+          <>
+            <Separator />
 
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {notionPages.map((item) => (
-                  <SidebarMenuItem key={item.id} className="w-full">
-                    <SidebarMenuButton
-                      className={cn(
-                        "block max-w-full overflow-hidden text-sidebar-foreground/70 font-medium",
-                        "whitespace-nowrap text-ellipsis"
-                      )}
-                    >
-                      {item.title}
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
+            <SidebarGroup>
+              <SidebarGroupLabel>{"Notion pages"}</SidebarGroupLabel>
+
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {notionPages.map((item) => (
+                    <SidebarMenuItem key={item.id} className="w-full">
+                      <SidebarMenuButton
+                        className={cn(
+                          "block max-w-full overflow-hidden text-sidebar-foreground/70 font-medium",
+                          "whitespace-nowrap text-ellipsis"
+                        )}
+                      >
+                        {item.title}
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </>
         )}
       </SidebarContent>
       <SidebarRail />
--- frontend/src/store/notion-pages.ts
+++ frontend/src/store/notion-pages.ts
@@ -2,6 +2,7 @@ import { create } from "zustand";
 import { useConfigStore } from "./config";
 import { NotionPage, NotionSimplePage } from "@/types";
 import { GetNotionPages } from "~wails/main/App";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type NotionPagesStore = {
   pages: Array<NotionPage>;
@@ -9,20 +10,33 @@ type NotionPagesStore = {
   getPages: () => Promise<void>;
 };
 
-export const useNotionPagesStore = create<NotionPagesStore>((set, get) => ({
-  pages: [],
-  getPages: async () => {
-    const pages = await GetNotionPages();
-    set({ pages });
-  },
-  getSimplifiedPages() {
-    return get().pages.map((page) => {
-      return {
-        id: page.id,
-        title: page.properties.title.title.map((t) => t.plain_text).join(" "),
-      };
-    });
-  },
-}));
+export const useNotionPagesStore = create(
+  persist<NotionPagesStore>(
+    (set, get) => ({
+      pages: [],
+
+      getPages: async () => {
+        const pages = await GetNotionPages();
+        set({ pages });
+      },
+
+      getSimplifiedPages() {
+        return get().pages.map((page) => {
+          return {
+            id: page.id,
+            title: page.properties.title.title
+              .map((t) => t.plain_text)
+              .join(" "),
+          };
+        });
+      },
+    }),
+
+    {
+      name: "notion-pages",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
 
 useConfigStore.subscribe(() => useNotionPagesStore.getState().getPages());
