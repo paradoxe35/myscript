a83b421c485a6580a546b47c90a93917fc939753
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2024-12-25 21:40:00 +0200

    Refactor ScriptReaderControllers to remove unused effects and streamline recording logic

--- frontend/src/components/app-header/script-reader-controllers.tsx
+++ frontend/src/components/app-header/script-reader-controllers.tsx
@@ -3,57 +3,18 @@ import { useActivePageStore } from "@/store/active-page";
 import { Button } from "../ui/button";
 import { BookOpenText, Play } from "lucide-react";
 import { useTranscriberStore } from "@/store/transcriber";
-import { useEffect } from "react";
 import { SRLanguagesModal } from "../sr-languages-modal";
-import { toast } from "sonner";
 
 export function ScriptReaderControllers(props: React.ComponentProps<"div">) {
   const transcriberStore = useTranscriberStore();
-
   const activePageStore = useActivePageStore();
+
   const activePage = activePageStore.page;
   const readMode = activePageStore.readMode;
 
   const handleLanguageSelected = (languageCode: string) => {
     transcriberStore.startRecording(languageCode);
   };
-
-  useEffect(() => {
-    transcriberStore.getRecordingStatus();
-  }, []);
-
-  useEffect(() => {
-    return transcriberStore.setOnTranscribedText((text) => {
-      console.log("Transcribed text:", text);
-      // toast.success("Transcription saved successfully!");
-    });
-  }, []);
-
-  // Stop recording when page changes
-  useEffect(() => {
-    transcriberStore.stopRecording();
-  }, [activePageStore.getPageId()]);
-
-  useEffect(() => {
-    return transcriberStore.setOnTranscribeError((error) => {
-      console.log("Transcription error:", error);
-      toast.error("Transcription error: " + error);
-    });
-  }, []);
-
-  useEffect(() => {
-    return transcriberStore.setOnRecordingStopped((autoStopped) => {
-      console.log("Recording stopped:", autoStopped);
-
-      if (autoStopped) {
-        toast.success("10 seconds of silence detected, stopping the read mode");
-      }
-    });
-  }, []);
-
-  useEffect(() => {
-    activePageStore.setReadMode(transcriberStore.isRecording);
-  }, [transcriberStore.isRecording]);
 
   if (!activePage) return null;
 
