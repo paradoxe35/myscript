490955fb6088b5e4208313054123d1c55ea17ea0
Paradoxe NGWASI <paradoxngwasi@gmail.com>
2025-01-07 12:39:36 +0200

    feat: add error boundary component and integrate into main application structure

---
+++ frontend/src/components/error-boundary.tsx
@@ -0,0 +1,37 @@
+import { PropsWithChildren } from "react";
+
+import { ErrorBoundary } from "react-error-boundary";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "./ui/button";
+import { WindowReloadApp } from "~wails-runtime";
+
+export function AppErrorBoundary(props: PropsWithChildren) {
+  return <ErrorBoundary fallback={<Error />}>{props.children}</ErrorBoundary>;
+}
+
+function Error() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Card className="max-w-md">
+        <CardHeader>
+          <CardTitle>An error has occurred.</CardTitle>
+          <CardDescription>
+            This might be a temporary issue. Please click the button below to
+            reload the application and try your action again.
+          </CardDescription>
+        </CardHeader>
+
+        <CardFooter>
+          <Button onClick={() => WindowReloadApp()}>Reload</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,11 @@ import { ThemeProvider } from "./components/theme-provider";
 import App from "./app";
 import { Toaster } from "./components/ui/sonner";
 
+import { AsyncPromptModalProvider } from "./components/async-prompt-modal";
+import { AppErrorBoundary } from "./components/error-boundary";
+
 import "./styles/style.css";
 import "./styles/prosemirror.css";
-import { AsyncPromptModalProvider } from "./components/async-prompt-modal";
 
 const container = document.getElementById("root");
 
@@ -15,10 +17,12 @@ const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <AsyncPromptModalProvider>
-        <App />
-        <Toaster />
-      </AsyncPromptModalProvider>
+      <AppErrorBoundary>
+        <AsyncPromptModalProvider>
+          <App />
+          <Toaster />
+        </AsyncPromptModalProvider>
+      </AppErrorBoundary>
     </ThemeProvider>
   </React.StrictMode>
 );
