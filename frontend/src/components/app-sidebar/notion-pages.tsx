865ac7688ccc46b8229dec69ee17f992a68e3fe9
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2025-01-11 20:45:56 +0200

    refactor: simplify NotionPages component structure; improve readability and remove unnecessary checks

--- frontend/src/components/app-sidebar/notion-pages.tsx
+++ frontend/src/components/app-sidebar/notion-pages.tsx
@@ -16,51 +16,51 @@ export function NotionPages() {
   const { notionPages, activePage, onNotionPageClick, refreshNotionPages } =
     useSidebarItemsContext();
 
+  if (notionPages.length < 1) {
+    return null;
+  }
+
   return (
     <>
-      {notionPages.length > 0 && (
-        <>
-          <Separator />
+      <Separator />
 
-          <SidebarGroup>
-            <div className="group/notion-refresh px-0 justify-between transition cursor-default">
-              <SidebarGroupLabel>{"Notion pages"}</SidebarGroupLabel>
+      <SidebarGroup>
+        <div className="group/notion-refresh px-0 justify-between transition cursor-default">
+          <SidebarGroupLabel>{"Notion pages"}</SidebarGroupLabel>
 
-              <SidebarGroupAction
-                className="opacity-0 group-hover/notion-refresh:opacity-100 transition hover:bg-white/10"
-                onClick={refreshNotionPages}
-              >
-                <RotateCw />
-              </SidebarGroupAction>
-            </div>
+          <SidebarGroupAction
+            className="opacity-0 group-hover/notion-refresh:opacity-100 transition hover:bg-white/10"
+            onClick={refreshNotionPages}
+          >
+            <RotateCw />
+          </SidebarGroupAction>
+        </div>
 
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {notionPages.map((item) => {
-                  const active =
-                    activePage?.__typename === "notion_page" &&
-                    activePage.page.id === item.id;
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {notionPages.map((item) => {
+              const active =
+                activePage?.__typename === "notion_page" &&
+                activePage.page.id === item.id;
 
-                  return (
-                    <SidebarMenuItem key={item.id} className="w-full">
-                      <SidebarMenuButton
-                        isActive={active}
-                        onClick={() => onNotionPageClick(item)}
-                        className={cn(
-                          "block max-w-full overflow-hidden text-sidebar-foreground/70 font-medium",
-                          "whitespace-nowrap text-ellipsis leading-3"
-                        )}
-                      >
-                        {item.title}
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  );
-                })}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </>
-      )}
+              return (
+                <SidebarMenuItem key={item.id} className="w-full">
+                  <SidebarMenuButton
+                    isActive={active}
+                    onClick={() => onNotionPageClick(item)}
+                    className={cn(
+                      "block max-w-full overflow-hidden text-sidebar-foreground/70 font-medium",
+                      "whitespace-nowrap text-ellipsis leading-3"
+                    )}
+                  >
+                    {item.title}
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              );
+            })}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
     </>
   );
 }
