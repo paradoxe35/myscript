113fc4764fb378ffd1b7367f458a4f1e68e9ba3f
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2025-02-02 01:15:56 +0200

    refactor: implement Synchronizer struct with options for device repository initialization

--- internal/synchronizer/synchronizer.go
+++ internal/synchronizer/synchronizer.go
@@ -1,1 +1,28 @@
 package synchronizer
+
+import "myscript/internal/repository"
+
+type Synchronizer struct {
+	deviceRepository *repository.DeviceRepository
+}
+
+// Option
+type Option func(s *Synchronizer)
+
+func WithDeviceRepository(deviceRepository *repository.DeviceRepository) Option {
+	return func(s *Synchronizer) {
+		s.deviceRepository = deviceRepository
+	}
+}
+
+// Init
+func NewSynchronizer(options ...Option) *Synchronizer {
+	s := &Synchronizer{}
+
+	for _, option := range options {
+		option(s)
+	}
+	return s
+}
+
+func (s *Synchronizer) StartScheduler() {}
