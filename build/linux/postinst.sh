3ce2c1faff9f69a8eb3588cce535e4ef629d1a5d
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2025-02-09 07:05:38 +0000

    refactor: enhance Linux package configuration with desktop entry and post-install scripts

---
+++ build/linux/postinst.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Update icon cache
+gtk-update-icon-cache -f -t /usr/share/icons/hicolor
---
+++ build/linux/prerm.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Remove desktop database entry
+update-desktop-database /usr/share/applications
